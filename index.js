c7994ea750ed46c3b167007c4bda17b3e7dc1600
nieyuyao <nieyuyao0826@hotmail.com>
2022-11-30 18:16:33 +0800

    feat: add clip example

--- index.js
+++ index.js
@@ -8,12 +8,33 @@ appDiv.innerHTML = `<h1>FabricJS Clip Example</h1>`;
 
 const canvas = new fabric.Canvas('c');
 
-let rect1 = new fabric.Rect({
+let rect = new fabric.Rect({
   left: 100,
   top: 100,
-  width: 100,
-  height: 75,
+  width: 300,
+  height: 300,
   fill: '#ffff00',
 });
 
-canvas.add(rect1);
+const maskCanvas = document.createElement('canvas');
+maskCanvas.width = 100;
+maskCanvas.height = 100;
+const maskCtx = maskCanvas.getContext('2d');
+maskCtx.beginPath();
+maskCtx.arc(50, 50, 50, 0, 2 * Math.PI);
+maskCtx.closePath();
+maskCtx.fillStyle = '#000';
+maskCtx.fill();
+
+canvas.add(rect);
+
+const url = maskCanvas.toDataURL();
+
+fabric.Image.fromURL(url, function (img) {
+  img.set({
+    left: 200,
+    top: 200,
+  });
+  img.globalCompositeOperation = 'destination-in';
+  canvas.add(img);
+});
